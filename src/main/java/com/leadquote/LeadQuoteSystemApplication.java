ec96f9b7eaf8a3ee99c01fba2158a55856e98ee4
Smohamedsameer <sameerkaleemi10@gmail.com>
2026-09-25 11:24:06 +0530

    updated

--- src/main/java/com/leadquote/LeadQuoteSystemApplication.java
+++ src/main/java/com/leadquote/LeadQuoteSystemApplication.java
@@ -1,4 +1,5 @@
 package com.leadquote;
+import java.util.TimeZone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class LeadQuoteSystemApplication {
     public static void main(String[] args) {
+         TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
         SpringApplication.run(LeadQuoteSystemApplication.class, args);
     }
 }
